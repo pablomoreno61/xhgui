8e3403eaa2deff4bb91f740f3c9f9adee050f96e
Lugovyi, Andrii <duhon@users.noreply.github.com>
2017-08-03 19:58:57 +0300

    only tideways

--- external/header.php
+++ external/header.php
@@ -1,43 +1,43 @@
 <?php
 
-\XH::autoStart();
+\XH::start();
 
 class XH
 {
-    private static $profileType;
     private static $mongoConfig = [
         'db.host' => 'mongodb://mongodb:27017',
         'db.db' => 'xhprof',
         'db.options' => []
     ];
 
-    static function start()
-    {
-        if (self::$profileType === 'tideways') {
-            tideways_enable(
-                TIDEWAYS_FLAGS_CPU | TIDEWAYS_FLAGS_MEMORY | TIDEWAYS_FLAGS_NO_SPANS | TIDEWAYS_FLAGS_NO_BUILTINS
-            );
-        } elseif (self::$profileType === 'uprofiler') {
-            uprofiler_enable(UPROFILER_FLAGS_CPU | UPROFILER_FLAGS_MEMORY);
-        } elseif (self::$profileType === xhprof) {
-            if (PHP_MAJOR_VERSION == 5 && PHP_MINOR_VERSION > 4) {
-                xhprof_enable(XHPROF_FLAGS_CPU | XHPROF_FLAGS_MEMORY | XHPROF_FLAGS_NO_BUILTINS);
-            } else {
-                xhprof_enable(XHPROF_FLAGS_CPU | XHPROF_FLAGS_MEMORY);
-            }
+    static function start() {
+        if (!extension_loaded('tideways')) {
+            error_log('xhgui - either extension xhprof, uprofiler or tideways must be loaded');
+            return;
         }
+        if (!extension_loaded('mongodb')) {
+            error_log('xhgui - extension mongo not loaded');
+            return;
+        }
+        tideways_enable(
+            TIDEWAYS_FLAGS_CPU | TIDEWAYS_FLAGS_MEMORY | TIDEWAYS_FLAGS_NO_SPANS | TIDEWAYS_FLAGS_NO_BUILTINS
+        );
+        register_shutdown_function(
+            function () {
+                \XH::stop();
+            }
+        );
     }
 
     static function stop()
     {
-        $disable = self::$profileType.'_disable';
-        $data['profile'] = $disable();
+        $data['profile'] = tideways_disable();
 
         ignore_user_abort(true);
         flush();
 
         $uri = array_key_exists('REQUEST_URI', $_SERVER) ? $_SERVER['REQUEST_URI'] : null;
-        if (empty($uri) && isset($_SERVER['argv'])) {
+        if (empty($uri)) {
             $cmd = basename($_SERVER['argv'][0]);
             $uri = $cmd . ' ' . implode(' ', array_slice($_SERVER['argv'], 1));
         }
@@ -67,29 +67,5 @@ class XH
         } catch (Exception $e) {
             error_log('xhgui - ' . $e->getMessage());
         }
-    }
-
-    static function autoStart() {
-        if (extension_loaded('tideways')) {
-            self::$profileType = 'tideways';
-        } elseif (extension_loaded('uprofiler')) {
-            self::$profileType = 'uprofiler';
-        } elseif (extension_loaded('xhprof')) {
-            self::$profileType = 'xhprof';
-        }
-        if (empty(self::$profileType)) {
-            error_log('xhgui - either extension xhprof, uprofiler or tideways must be loaded');
-            return;
-        }
-        if (!extension_loaded('mongo') && !extension_loaded('mongodb')) {
-            error_log('xhgui - extension mongo not loaded');
-            return;
-        }
-        \XH::start();
-        register_shutdown_function(
-            function () {
-                \XH::stop();
-            }
-        );
     }
 }
