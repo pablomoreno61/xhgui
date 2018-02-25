1520a05f3379eccda040acd40657452b4908d825
Lugovyi, Andrii <duhon@users.noreply.github.com>
2018-02-25 22:58:28 +0200

    Update api.php

--- webroot/api.php
+++ webroot/api.php
@@ -1,17 +1,42 @@
 <?php
 
+function exception_error_handler($severity, $message, $file, $line) {
+    throw new ErrorException($message, 0, $severity, $file, $line);
+}
+set_error_handler("exception_error_handler");
+
 try {
-    $data = file_get_contents('php://input');
-
-    $data['meta']['request_ts'] = new MongoDB\BSON\UTCDateTime(DateTime::createFromFormat('U', $data['meta']['request_ts']));
-    $data['meta']['request_ts_micro'] = new MongoDB\BSON\UTCDateTime(preg_replace('/\D/', '',  $data['meta']['request_ts_micro']));
-    $data['meta']['simple_url'] = preg_replace('/\=\d+/', '', $data['meta']['simple_url']);
-
+	$_SERVER = $_POST['meta']['server'];
+	
+	$uri = array_key_exists('REQUEST_URI', $_SERVER) ? $_SERVER['REQUEST_URI'] : null;
+	if (empty($uri)) {
+		$cmd = basename($_SERVER['argv'][0]);
+		$uri = $cmd . ' ' . implode(' ', array_slice($_SERVER['argv'], 1));
+	}
+	
+	$time = empty($_SERVER['REQUEST_TIME']) ? (new DateTime())->getTimestamp() : $_SERVER['REQUEST_TIME'];
+    $milliseconds = empty($_SERVER['REQUEST_TIME_FLOAT']) ? (new DateTime())->getTimestamp() : $_SERVER['REQUEST_TIME_FLOAT'];
+	
     $manager = new MongoDB\Driver\Manager('mongodb://mongodb:27017');
     $bulk = new MongoDB\Driver\BulkWrite;
+	
+	$data = [
+		'profile' => $_POST['profile'],
+		'meta' => [
+			'url' => $uri,
+			'SERVER' => $_SERVER,
+			'get' => $_POST['meta']['get'] ?? [],
+			'env' => $_POST['meta']['env'],
+			'simple_url' => preg_replace('/\d+$/', '', $uri),
+			'request_ts' => new MongoDB\BSON\UTCDateTime(DateTime::createFromFormat('U', $time)),
+			'request_ts_micro' => new MongoDB\BSON\UTCDateTime(preg_replace('/\D/', '',  $milliseconds)),
+			'request_date' => date('Y-m-d', $time),
+		]
+	];
+		
     $bulk->insert($data);
-    $cursor = $manager->executeBulkWrite('xhprof.results', $bulk);
-} catch (Exception $e) {
-    header("HTTP/1.1 500 Internal Server Error");
-    echo 'xhgui - ' . $e->getMessage();
+    $manager->executeBulkWrite('xhprof.results', $bulk);
+} catch (Throwable $e) {
+    header('HTTP/1.1 500 Internal Server Error');
+	error_log($e->getMessage().':'.$e->getLine());
 }
